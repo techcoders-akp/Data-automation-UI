955d89dc6c91acce1a255e9190751877b9855ae0
Amit Chattopadhyay <1905833@kiit.ac.in>
2022-12-21 20:21:00 +0530

    sidebar and navbar basic ui

--- src/app/Components/UI WINDOW/UIRenderer.jsx
+++ src/app/Components/UI WINDOW/UIRenderer.jsx
@@ -1,8 +1,31 @@
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import SearchIcon from '@mui/icons-material/Search';
+import { Chip } from '@mui/material';
+import TextField from "@mui/material/TextField";
 
 const UIRenderer = () => {
   return (
+    // 
     <>
-    <h1>Priyesh Was Here</h1>
+    <div className='flexColumn' >
+      <button className='flexButton'><AccountBalanceIcon/></button>
+      <button className='flexButton1'><AccountBalanceIcon/></button>
+      <button className='flexButton1'><AccountBalanceIcon/></button>
+      <button className='flexButton1'><AccountBalanceIcon/></button>
+      <button className='flexButton1'><AccountBalanceIcon/></button>
+    
+
+    </div>
+
+    <div class="navbar">
+        <button className='flexButton2'><AccountBalanceIcon/></button>
+        <button className='flexButton2'><AccountBalanceIcon/></button>
+        <button className='flexButton2'><AccountBalanceIcon/></button>
+        <button className='flexButton3'><SearchIcon/></button>
+
+
+</div>
+<h1>Automation</h1>
     </>
   )
 }
